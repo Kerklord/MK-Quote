a9887ef72d36224c22cbf9199801a27904f877a3
Kerk <114801065+Kerklord@users.noreply.github.com>
2025-07-27 00:46:29 -0400

    Update QuoteCaculator.py

--- QuoteCaculator.py
+++ QuoteCaculator.py
@@ -154,12 +154,14 @@ if show_quote:
     st.markdown("\n".join(quote_lines))
 
 if show_gp_prompt:
-    pw = st.text_input("Enter password to view GP-Cal results:", type="password", key="gp_pw", placeholder="Enter password and press Enter")
-    submit_pw = st.button("Submit Password")
-    if submit_pw and pw == "5150":
-        final_total, profit_amount, quote = calculate_quote(qty, design_paid, packaging_design_paid, commercial, packaging, keychain, custom_parts_qty, discount_addons, part_sourcing, package_tier)
-        st.markdown(quote)
-    elif submit_pw and pw:
-        st.error("Incorrect password.")
+    with st.form("gp_cal_form"):
+        pw = st.text_input("Enter password to view GP-Cal results:", type="password")
+        submitted = st.form_submit_button("Submit Password")
+        if submitted:
+            if pw == "5150":
+                final_total, profit_amount, quote = calculate_quote(qty, design_paid, packaging_design_paid, commercial, packaging, keychain, custom_parts_qty, discount_addons, part_sourcing, package_tier)
+                st.markdown(quote)
+            else:
+                st.error("Incorrect password.")
 
 st.markdown("\n---\n<center>Qazer Inc. © 2025 All Rights Reserved.</center>", unsafe_allow_html=True)
